c7cd08d15f444643b0d9c77c53b960069bdf3fd6
jokerwoow <jokerwoow1997@gmail.com>
2018-02-27 20:10:59 +0200

    oleksiy mydak

--- TelegramBot.py
+++ TelegramBot.py
@@ -4,53 +4,68 @@ from bs4 import BeautifulSoup
 import telebot 
 import datetime
 from time import sleep
+import random
 TOKEN='519695376:AAGgB9LqmRsiGPyYnDSWNCbMXxJqxRFBHas'
 bot=telebot.TeleBot(TOKEN)
 
 #@bot.message_handler(content_types=["text"])
 #-226511191
 url = "http://asu.pnu.edu.ua/cgi-bin/timetable.cgi"
-def brous(day,weekDay,tmTd):
-	response = requests.post(url,data={'group':'ІНФ-32'.encode('cp1251'),'sdate':day,'btn btn-success':'true','faculty':'1002'})
-	text=str(response.text.encode('iso-8859-1').decode('cp1251'))
-	soup = BeautifulSoup(text,'html.parser')
-	
-	schedule=[] 
-	time=[]
-	numb=[]
-	things=[]
-	
-	text=''
+lox=['Лашара я тебе хейчу','Пздц ти лох','Льоха підр','Боже льоха чого ти такий мудак?',
+'Ой та закрийся вже','Всьо тобі пизда','Тікай з села мудак по тебе виїхали']
 
-	divTag = soup.find("div", {"class": "container"})
+today=['які пари сьогодні','які пари','які сьогодні пари']
+
+tomorrow=['які пари завтра','які завтра пари','які в нас пари завтра','які пари взагалі завтра']
+
+dictt={'комп. мережі.':'Петришин Михайло Любомирович','моідо':'Мазуренко Віктор Володимирович','педагогіка.':'Стинська Вікторія Володимирівна',
+'менеджмент.': 'Гречаник Наталія Юріївна','обробка зображ.':'Косаревич Ростислав Ярославович','комп. математика.':'Костишин Любов Павлівна',
+'паралельні. обчис.':'Горєлов Віталій Олевтинович','операційні. системи.':' Гейко Орест Ярославович'}
+oleksiy=random.randrange(0,len(lox))
+def brous(day,weekDay,tmTd):
+	try:
+		response = requests.post(url,data={'group':'ІНФ-32'.encode('cp1251'),'sdate':day,'btn btn-success':'true','faculty':'1002'})
+		text=str(response.text.encode('iso-8859-1').decode('cp1251'))
+		soup = BeautifulSoup(text,'html.parser')
+		
+		schedule=[] 
+		time=[]
+		numb=[]
+		things=[]
+		
+		text=''
 	
-	for tag in divTag:
-	    tdTags = tag.find_all("td")
-	    for tag in tdTags:
-	        schedule.append(tag.text)   
-	a=len(schedule)        
-	size=a-(a/3)
-	n=0
-	if len(schedule)>1:
-		for i in range(len(schedule)-int(size)):
-			numb.append(schedule[n])
-			n=n+1
-			time.append(schedule[n][0:5]+'-'+schedule[n][5:10])
-			n=n+1
-			things.append(schedule[n])
-			n=n+1
-		for i in range(int(a/3)):
-			if things[i]=='':
-				numb[i]=''
-				time[i]=''
-				text=''
-			else:	
-				text=text+numb[i]+' пара ' +'('+time[i]+')'+'\n'+things[i]+'\n\n'
-		text='Розклад на '+day+'('+weekDay+')'+'\n\n'+text	
-	elif weekDay=='Saturday' or weekDay=='Sunday':
-		text=tmTd+'Вихідний' 			
-	else:
-		text=tmTd+'немає пар юху 	👍\n Або сайт з розкладом накрився 👎'	
+		divTag = soup.find("div", {"class": "container"})
+		
+		for tag in divTag:
+		    tdTags = tag.find_all("td")
+		    for tag in tdTags:
+		        schedule.append(tag.text)   
+		a=len(schedule)        
+		size=a-(a/3)
+		n=0
+		if len(schedule)>1:
+			for i in range(len(schedule)-int(size)):
+				numb.append(schedule[n])
+				n=n+1
+				time.append(schedule[n][0:5]+'-'+schedule[n][5:10])
+				n=n+1
+				things.append(schedule[n])
+				n=n+1
+			for i in range(int(a/3)):
+				if things[i]=='':
+					numb[i]=''
+					time[i]=''
+					text=''
+				else:	
+					text=text+numb[i]+' пара ' +'('+time[i]+')'+'\n'+things[i]+'\n\n'
+			text='Розклад на '+day+'('+weekDay+')'+'\n\n'+text	
+		elif weekDay=='Saturday' or weekDay=='Sunday':
+			text=tmTd+'Вихідний' 			
+		else:
+			text=tmTd+'немає пар юху 👍'	
+	except:
+		text='Сайт з розкладом не працює'
 	return text	
 	'''elif weekDay>4:	
 		text='Вихідниииииий'''
@@ -109,9 +124,8 @@ def help(message):
 @bot.message_handler(content_types=["text"])
 def text(message):
 	now= datetime.datetime.now()
-	today=['які пари сьогодні','які пари','які сьогодні пари']
-	tomorrow=['які пари завтра','які завтра пари','які в нас пари завтра','які пари взагалі завтра']
-	dictt={'комп. мережі.':'Петришин Михайло Любомирович','моідо':'Мазуренко Віктор Володимирович','педагогіка.':'Стинська Вікторія Володимирівна','менеджмент.': 'Гречаник Наталія Юріївна','обробка зображ.':'Косаревич Ростислав Ярославович','комп. математика.':'Костишин Любов Павлівна','паралельні. обчис.':'Горєлов Віталій Олевтинович','операційні. системи.':' Гейко Орест Ярославович'}
+	
+	
 	for i in today:
 		if message.text.lower()==i:
 			day=str(now.day)+'.'+str(now.month)+'.'+str(now.year)
@@ -126,7 +140,8 @@ def text(message):
 		if i in message.text.lower():
 			bot.send_message(message.chat.id,dictt[i])
 			bot.send_message(message.chat.id,'тадам',reply_markup=markup)
-			
+	if message.from_user.first_name=='Олексій' and message.from_user.last_name=='Пашкевич':
+		bot.send_message(message.chat.id,lox[oleksiy]) 		
 '''@bot.callback_query_handler(func=lambda c: c.data)
 def pages(c):
     """Редактируем сообщение каждый раз, когда пользователь переходит по
