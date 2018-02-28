4a938ccbf30bf074ab5949ba7b20abb8dc5f5f79
jokerwoow <jokerwoow1997@gmail.com>
2018-02-28 17:51:15 +0200

    new month

--- TelegramBot.py
+++ TelegramBot.py
@@ -95,7 +95,7 @@ def tommorrow(message):
 		weekDay=datetime.datetime(now.year,now.month,now.day+1).strftime('%A')
 	except:
 		weekDay=datetime.datetime(now.year,now.month+1,1).strftime('%A')
-		day=str(1)+'.'+str(now.month)+'.'+str(now.year)	
+		day=str(1)+'.'+str(now.month+1)+'.'+str(now.year)	
 	tmTd='Завтра '
 	bot.send_message(message.chat.id,brous(day,weekDay,tmTd))
 @bot.message_handler(commands=['Викладач'])
